68f8fd3ac8eba7a5c51225ca8451329009912747
ekaterina <kat_klepko@mail.ru>
2023-03-05 13:56:49 +0300

    commit3

--- Program.cs
+++ Program.cs
@@ -39,6 +39,64 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(0, 10);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j],3} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+// void CheckArray(int[,] array)
+// {
+//     Console.WriteLine("Введите число: ");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     bool find = false;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (num == array[i, j])
+//             {
+//                 find = true;
+//                 break;
+//             }
+//             if (find)
+//                 Console.WriteLine($"Элемент находится на позиции {i}, {j}");
+//             else
+//                 Console.WriteLine("Такого элемента в массиве нет");
+//         }
+//     }
+// }
+
+
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int cols = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, cols];
+// FillArray(array);
+// PrintArray(array);
+// CheckArray(array);
+
+
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
 void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -56,29 +114,21 @@ void PrintArray(int[,] array)
     }
 }
 
-void CheckArray(int[,] array)
+void Average (int[,] array)
 {
-    Console.WriteLine("Введите число: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    bool find = false;
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (array[i, j] == num)
-            {
-                find = true;
-                break;
-            }
-            if (find)
-                Console.WriteLine($"Элемент находится на позиции {i}, {j}");
-            else
-                Console.WriteLine("Такого элемента в массиве нет");
+        double avrg = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {   
+            avrg = avrg + array[i, j];
+            avrg = avrg / (i+1);
+            Console.Write(avrg + "; ");
         }
+        
     }
-
+    
 }
-
 
 Console.WriteLine("Введите количество строк");
 int rows = Convert.ToInt32(Console.ReadLine());
@@ -87,5 +137,4 @@ int cols = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[rows, cols];
 FillArray(array);
 PrintArray(array);
-CheckArray(array);
-
+Average (array);
